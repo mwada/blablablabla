c1a320014b169ad9740dbe2cfd3c16068cbdf2cd
Marcos Wada <marcos.wada@gmail.com>
2016-08-28 19:46:20 -0300

    adding utils for json loading

---
+++ src/main/java/com/vivareal/spotippos/repository/memory/MemoryRepositoryLoader.java
@@ -0,0 +1,55 @@
+package com.vivareal.spotippos.repository.memory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vivareal.spotippos.model.Property;
+import com.vivareal.spotippos.model.Province;
+
+public class MemoryRepositoryLoader {
+
+	private final static String PROVINCE_FILE = "provinces.json";
+
+	private final static String PROPERTY_FILE = "properties.json";
+	
+	protected Map<String, Province> loadProvinceFile() throws JsonParseException, JsonMappingException, IOException {
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource(PROVINCE_FILE).getFile());
+		return new ObjectMapper().readValue(file, new TypeReference<Map<String, Province>>() {
+		});
+	}
+
+	protected List<Property> loadPropertyFile() throws JsonParseException, JsonMappingException, IOException {
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource(PROPERTY_FILE).getFile());
+		Properties properties = new ObjectMapper().readValue(file, new TypeReference<Properties>() {
+		});
+		return properties.getProperties();
+	}
+	
+
+	public static class Properties {
+		private Integer totalProperties;
+
+		private List<Property> properties;
+
+		public Integer getTotalProperties() {
+			return totalProperties;
+		}
+
+		public void setTotalProperties(Integer totalProperties) {
+			this.totalProperties = totalProperties;
+		}
+
+		public List<Property> getProperties() {
+			return properties;
+		}
+	}
+
+}
---
+++ src/test/java/com/vivareal/spotippos/repository/memory/MemoryRepositoryLoaderTest.java
@@ -0,0 +1,39 @@
+package com.vivareal.spotippos.repository.memory;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.vivareal.spotippos.model.Property;
+import com.vivareal.spotippos.model.Province;
+
+public class MemoryRepositoryLoaderTest {
+
+	private MemoryRepositoryLoader loader;
+
+	@Before
+	public void before() {
+		loader = new MemoryRepositoryLoader();
+	}
+
+	@Test
+	public void testLoadProvinceFile() throws JsonParseException, JsonMappingException, IOException {
+		Map<String, Province> provinces = loader.loadProvinceFile();
+		assertThat(provinces.keySet(), hasSize(6));
+	}
+
+	@Test
+	public void testLoadPropertyFile() throws JsonParseException, JsonMappingException, IOException {
+		List<Property> properties = loader.loadPropertyFile();
+		assertThat(properties, hasSize(8000));
+	}
+
+}
