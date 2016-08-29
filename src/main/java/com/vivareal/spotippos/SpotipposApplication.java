c60adb71fa052fa5459fef1734a4d9826eaa2c3b
Marcos Wada <marcos.wada@gmail.com>
2016-08-29 19:43:05 -0300

    changing application to spring-boot

---
+++ src/main/java/com/vivareal/spotippos/SpotipposApplication.java
@@ -0,0 +1,13 @@
+package com.vivareal.spotippos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotipposApplication {
+
+	public static void main(String[] args) throws Exception {
+	    SpringApplication.run(SpotipposApplication.class, args);
+	}
+
+}
